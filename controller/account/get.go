33c81a0686899240b96a3c894ef1b10628e59c15
GuilhermeCoutinho <guilhermejcoutinho@gmail.com>
2021-05-05 08:17:13 -0300

    Add get account route

--- controller/account/get.go
+++ controller/account/get.go
@@ -2,10 +2,35 @@ package account
 
 import (
 	"context"
+	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/guilhermeCoutinho/payment-system/server/http/wrapper"
 )
 
-func (m *Account) Get(ctx context.Context, args *struct{}, vars *struct{}) (*struct{}, *wrapper.HandlerError) {
-	return nil, nil
+type GetAccountParams struct {
+	ID *uuid.UUID `json:"id, a"`
+}
+
+func (c *GetAccountParams) Validate() error {
+	if c.ID == nil {
+		return ErrMissingRequestParam
+	}
+	return nil
+}
+
+func (a *Account) Get(ctx context.Context, args *struct{}, vars *GetAccountParams) (*CreateAccountResponse, *wrapper.HandlerError) {
+	err := vars.Validate()
+	if err != nil {
+		return nil, &wrapper.HandlerError{Err: err, StatusCode: http.StatusBadRequest}
+	}
+
+	account, err := a.dal.Account.Get(ctx, *vars.ID)
+	if err != nil {
+		return nil, &wrapper.HandlerError{Err: err, StatusCode: http.StatusNotFound}
+	}
+
+	return &CreateAccountResponse{
+		Account: account,
+	}, nil
 }
